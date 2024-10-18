f2326b8c52584d23dbd0d94e5c2baaea9a265376
Hannes Karppila <hannes.karppila@gmail.com>
2024-10-18 18:03:25 +0300

    Add clap derive to the downloader config

--- crates/downloader/src/config.rs
+++ crates/downloader/src/config.rs
@@ -1,17 +1,23 @@
 use serde::{Deserialize, Serialize};
+use clap::Parser;
 
+#[derive(Parser)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
     /// URL to the Ethereum RPC endpoint.
     /// For example `https://sepolia.infura.io/v3/API_KEY_HERE`.
+    #[clap(long, env = "ETHEREUM_RPC_URL")]
     pub ethereum_rpc_url: String,
     /// URL to the Ethereum consensus/beacon layer RPC endpoint.
     /// For example `https://ethereum-sepolia.core.chainstack.com/beacon/API_KEY_HERE`.
+    #[clap(long, env = "BEACON_RPC_URL")]
     pub beacon_rpc_url: String,
     /// Contract to monitor for new blobs.
     /// For example `0xB0B3682211533cB7C1a3Bcb0e0Dd4349fF000d75`.
+    #[clap(long, env = "BLOB_CONTRACT")]
     pub blob_contract: String,
     /// Block to start from.
+    #[clap(long, env = "START_BLOCK")]
     pub start_block: u64,
 }
 
